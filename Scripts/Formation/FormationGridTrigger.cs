5cf48108a01bf96c6df5adc8252c0d11f5c0ebff
Goodgulf281 <dave.kerkhove@gmail.com>
2017-07-29 14:09:17 +0200

    Update FormationGridTrigger.cs

--- Scripts/Formation/FormationGridTrigger.cs
+++ Scripts/Formation/FormationGridTrigger.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Pathfinding.Util;
+//using Pathfinding.Util; // Code from A*Pathfinding
 using com.t7t.utilities;
 
 namespace com.t7t.formation
@@ -134,7 +134,7 @@ namespace com.t7t.formation
         {
             if (drawGizmos)
             {
-                Draw.Gizmos.CircleXZ(transform.position, range, Color.yellow);
+                //Draw.Gizmos.CircleXZ(transform.position, range, Color.yellow);
 
             }
         }
